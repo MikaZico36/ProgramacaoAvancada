78138e7ebc0a512148b41b136a8970227125d7cb
Miguel Monteiro <mmonteiro03611@gmail.com>
2025-05-06 20:31:09 +0100

    JsonModelClass está quase feito
    Falta corrigir testes e falta ter o JsonEnum

---
+++ src/classes/JsonModel.kt
@@ -0,0 +1,55 @@
+package classes
+
+import classes.interfaces.JsonValue
+import classes.primitive.JsonBoolean
+import classes.primitive.JsonNull
+import classes.primitive.JsonNumber
+import classes.primitive.JsonString
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
+
+class JsonModel {
+
+    fun toJsonModel(value:Any?): JsonValue {
+
+        return when (value){
+            null -> JsonNull(value)
+            is String -> JsonString(value)
+            is Number -> JsonNumber(value)
+            is Boolean -> JsonBoolean(value)
+            is List<*> -> JsonArray(value.map { toJsonModel(it) })
+            //is Enum...
+            is Map<*,*> -> {
+                if (value.keys.all {it is String}) {
+                    val pairs = value.entries.map { (key,value) -> key as String to toJsonModel(value) }
+                    JsonObject(pairs)
+                } else {
+                    throw IllegalArgumentException("Only Map<String, *> supported!")
+                }
+            }
+
+            else -> {
+                val kClass = value::class
+                if (kClass.isData){
+                    val props = kClass.memberProperties.map{ prop ->
+                        prop.isAccessible = true
+                        val name = prop.name
+                        val value = prop.getter.call(value)
+                        name to toJsonModel(value)
+                    }
+                    JsonObject(props)
+
+                }else {
+                    throw IllegalArgumentException("Type not Supported!")
+                }
+            }
+
+        }
+
+
+
+    }
+
+
+
+}
--- src/classes/interfaces/JsonVisitor.kt
+++ src/classes/interfaces/JsonVisitor.kt
@@ -7,11 +7,56 @@ import classes.primitive.JsonNull
 import classes.primitive.JsonNumber
 import classes.primitive.JsonString
 
+
+/**
+ * Interface que representa os posiveis visitors do JsonValues
+ *
+ * Implementações desta interface:
+ * - [ValidatorVisitor]
+ *
+ * @see JsonValue
+ */
 interface JsonVisitor {
+
+    /**
+    * Visita um JSONObject ({ ... }) e executa uma operação sobre ele.
+    *
+    * @param jsonObject o objeto JSON a ser visitado.
+    */
     fun visitObject(jsonObject: JsonObject)
+
+    /**
+    * Visita um JSONArray  ([ ... ]) e executa uma operação sobre ele.
+    *
+    * @param jsonArray o array JSON a ser visitado.
+    */
     fun visitArray(jsonArray: JsonArray)
+
+    /**
+    * Visita uma JSONString e executa uma operação sobre ela.
+    *
+    *  @param jsonString o valor do tipo string a ser visitado.
+    */
     fun visitString(jsonString: JsonString)
+
+    /**
+    * Visita um JSONValue boolean (true ou false) e executa uma operação sobre ele.
+    *
+    * @param jsonBoolean o valor boolean a ser visitado.
+    */
     fun visitBoolean(jsonBoolean: JsonBoolean)
+
+    /**
+    * Visita um valor JSONValue nulo (null) e executa uma operação sobre ele.
+    *
+    * @param jsonNull o valor nulo a ser visitado.
+    */
     fun visitNull(jsonNull: JsonNull<*>)
+
+    /**
+    * Visita um JSON Number (inteiro, decimal, potência,...) e executa uma operação sobre ele.
+    *
+    * @param jsonNumber o valor numérico a ser visitado.
+    */
     fun visitNumber(jsonNumber: JsonNumber)
 }
